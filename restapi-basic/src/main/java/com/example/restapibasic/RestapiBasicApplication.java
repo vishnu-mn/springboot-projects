8019bd348b297bf3d844b30eb6a623de29a2cd13
vishnu.m321@gmail.com <vishnu.m321@gmail.com>
2025-03-29 13:43:17 +1100

    HelloWorld implemented

---
+++ restapi-basic/src/main/java/com/example/restapibasic/RestapiBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapibasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiBasicApplication.class, args);
+    }
+
+}
---
+++ restapi-basic/src/test/java/com/example/restapibasic/RestapiBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapibasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
