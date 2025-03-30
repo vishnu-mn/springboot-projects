090cf44458356177db2648d010b82f8c47bbcf56
vishnu.m321@gmail.com <vishnu.m321@gmail.com>
2025-03-30 15:33:18 +1100

    implemented returning list of students

--- restapi-basic/src/main/java/com/example/restapibasic/controller/StudentController.java
+++ restapi-basic/src/main/java/com/example/restapibasic/controller/StudentController.java
@@ -4,11 +4,21 @@ import com.example.restapibasic.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("student")
     public Student getStudent(){
         Student student = new Student(1,"samplefname","samplelname");
         return student;
+    }
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student(1,"samplefname","samplelname"));
+        students.add(new Student(2,"samplefname2","samplelname2"));
+        return students;
     }
 }
